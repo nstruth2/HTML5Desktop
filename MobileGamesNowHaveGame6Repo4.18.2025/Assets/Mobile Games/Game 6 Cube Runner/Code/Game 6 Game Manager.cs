0cca7311d875328f48515f9dc280ba2111cdf054
nstruth <nstruth1@gmail.com>
2025-04-18 15:01:05 -0400

    Added didfficulty increase

--- MobileGamesNowHaveGame6Repo4.18.2025/Assets/Mobile Games/Game 6 Cube Runner/Code/Game 6 Game Manager.cs
+++ MobileGamesNowHaveGame6Repo4.18.2025/Assets/Mobile Games/Game 6 Cube Runner/Code/Game 6 Game Manager.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -11,61 +10,86 @@ public class Game6GameManager : MonoBehaviour
     public float maxSpawnPointX;
     public GameObject menuPanel;
     public static Game6GameManager instance;
-    int score = 0;
-    int highScore = 0;
+
+    public float initialSpeed = -12f;
+    public float maxSpeed = -400f;
+    private float currentSpeed;
+
     public Text scoreText;
     public Text highScoreText;
-    bool gameStarted = false;
+
+    private int score = 0;
+    private int highScore = 0;
+    private bool gameStarted = false;
 
     private void Awake()
     {
-        if(instance==null)
-        {
+        if (instance == null)
             instance = this;
-        }
     }
+
     void Start()
     {
-        if(PlayerPrefs.HasKey("highScore"))
+        currentSpeed = initialSpeed;
+
+        if (PlayerPrefs.HasKey("highScore"))
         {
             highScore = PlayerPrefs.GetInt("highScore");
             highScoreText.text = "HIGH SCORE: " + highScore.ToString();
         }
     }
+
     void Update()
     {
-        if(Input.anyKeyDown&&!gameStarted)
+        if (Input.anyKeyDown && !gameStarted)
         {
-            menuPanel.gameObject.SetActive(false);
+            menuPanel.SetActive(false);
             scoreText.gameObject.SetActive(true);
-            StartCoroutine("SpawnEnemies");
+            StartCoroutine(SpawnEnemies());
+            StartCoroutine(IncreaseSpeedOverTime());
             gameStarted = true;
         }
     }
+
     IEnumerator SpawnEnemies()
     {
-        while(true)
+        while (true)
         {
             yield return new WaitForSeconds(0.8f);
             Spawn();
         }
     }
+
+    IEnumerator IncreaseSpeedOverTime()
+    {
+        while (currentSpeed > maxSpeed)
+        {
+            yield return new WaitForSeconds(1f);
+            currentSpeed -= 5f;
+        }
+    }
+
     public void Spawn()
     {
-        float randomSpawnX = Random.Range(-maxSpawnPointX,maxSpawnPointX);
+        float randomSpawnX = Random.Range(-maxSpawnPointX, maxSpawnPointX);
         Vector3 enemySpawnPos = spawnPoint.position;
         enemySpawnPos.x = randomSpawnX;
-        Instantiate(enemy,enemySpawnPos,Quaternion.identity);
+
+        GameObject newEnemy = Instantiate(enemy, enemySpawnPos, Quaternion.identity);
+        newEnemy.GetComponent<Game6Enemy>().speed = currentSpeed;
     }
+
     public void Restart()
     {
-        if(score>highScore)
+        if (score > highScore)
         {
             highScore = score;
-            PlayerPrefs.SetInt("highScore",highScore);
+            PlayerPrefs.SetInt("highScore", highScore);
         }
+
         SceneManager.LoadScene("Cube Runner");
     }
+
     public void ScoreUp()
     {
         score++;
