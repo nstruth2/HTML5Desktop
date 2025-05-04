39743f128681880fe3d2bf1162215080216c8313
nstruth <nstruth1@gmail.com>
2025-05-04 12:48:13 -0400

    Working on Game 5

--- Assets/Mobile Games/Game 5 Ball Smash/Code/Game5GameManager.cs
+++ Assets/Mobile Games/Game 5 Ball Smash/Code/Game5GameManager.cs
@@ -1,33 +1,60 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Game5GameManager : MonoBehaviour
 {
     public GameObject winText;
     public GameObject restartButton;
+    public Text timeText; // ✅ Legacy Text
+    private float elapsedTime = 0f;
+    private bool gameRunning = true;
+
     int score = 0;
-    // Update is called once per frame
+
     void Update()
     {
-        
+        if (gameRunning)
+        {
+            elapsedTime += Time.deltaTime;
+            timeText.text = FormatTime(elapsedTime);
+        }
     }
+
     public void ScoreUp()
     {
         score++;
-        if(score >= 4)
+        if (score >= 4)
         {
             Win();
         }
-        void Win()
-        {
-            winText.SetActive(true);
-            restartButton.SetActive(true);
-        }
     }
+
+    void Win()
+    {
+        winText.SetActive(true);
+        restartButton.SetActive(true);
+        gameRunning = false;
+
+        // Save raw time as high-precision decimal string
+        decimal timeRaw = (decimal)elapsedTime;
+        PlayerPrefs.SetString("Game5_SubmitTimeRaw", timeRaw.ToString());
+
+        // Save formatted time string for display
+        PlayerPrefs.SetString("Game5_SubmitTime", FormatTime(elapsedTime));
+    }
+
     public void Restart()
     {
         SceneManager.LoadScene("Game 5 Ball Smash");
+    }
+
+    string FormatTime(float time)
+    {
+        int minutes = (int)(time / 60f);
+        int seconds = (int)(time % 60f);
+        int centiseconds = (int)((time * 100f) % 100f);
+        return $"{minutes:00}:{seconds:00}.{centiseconds:00}";
     }
 }
